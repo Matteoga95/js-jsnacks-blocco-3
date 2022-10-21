dfe15b111cad93306d3f1f6cb51b5674157309ee
matteoga95 <gallo.work95@gmail.com>
2022-10-21 12:37:47 +0200

    snack3

---
+++ snack3/assets/js/main.js
@@ -0,0 +1,21 @@
+// Snack 3 @qui
+// Crea un array di oggetti che rappresentano degli animali.
+// Ogni animale ha un nome, una famiglia e una classe.
+
+const animals = [
+    { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'gallina', famiglia: 'fasianidi', classe: 'non-mammiferi' },
+    { nome: 'gatto', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'passserotto', famiglia: 'uccelli', classe: 'mammiferi' },
+    { nome: 'coccodrillo', famiglia: 'rettili', classe: 'non-mammiferi' }
+  ]
+// Crea un nuovo array con la lista dei mammiferi.
+
+const mammiferi = animals.filter(function(animale){
+if (animale.classe ==="mammiferi"){
+    return animale
+}
+})
+
+console.log(mammiferi);
